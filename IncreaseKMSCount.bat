02671ee22979928f182d394cd070ba7173753bb2
Sohrab Nz <71740465+SohrabN@users.noreply.github.com>
2022-12-23 13:09:12 -0500

    Update IncreaseKMSCount.bat

--- IncreaseKMSCount.bat
+++ IncreaseKMSCount.bat
@@ -10,6 +10,7 @@ xcopy "7B296FB0-376B-497e-B012-9C450E1B7327-5P-0.C7483456-A289-439d-8115-601632D
 xcopy "7B296FB0-376B-497e-B012-9C450E1B7327-5P-1.C7483456-A289-439d-8115-601632D005A0" "%systemroot%\system32\*" /H /R /K /Y
 sc start sppsvc
 cscript.exe "%systemroot%\system32\slmgr.vbs" /skms %1
+ping 127.0.0.1 -n 5 > nul
 cscript.exe "%systemroot%\system32\slmgr.vbs" /ipk FJ82H-XT6CR-J8D7P-XQJJ2-GPDD4
 cscript.exe "%systemroot%\system32\slmgr.vbs" /ato
 sc stop sppsvc
